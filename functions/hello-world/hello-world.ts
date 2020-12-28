3e5460f4aa62b51c79bee3dd560ef4047684c875
Tom <tpjnorton@hotmail.co.uk>
2020-12-28 18:41:06 +0100

    add remaning things

--- functions/hello-world/hello-world.ts
+++ functions/hello-world/hello-world.ts
@@ -1,17 +1,16 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
+import { APIGatewayEvent } from 'aws-lambda';
+
+const handler = async (event: APIGatewayEvent) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = event?.queryStringParameters?.name || 'World';
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
