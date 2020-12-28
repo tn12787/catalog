19e422333135582d4c5255ba1227947f6bc0573d
Tom <tpjnorton@hotmail.co.uk>
2020-12-28 21:28:03 +0100

    purge netlify from repo

---
+++ src/firebase-details.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+
+// Add the Firebase products that you want to use
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCGX4CHOsUmfACG1Ybqty3jlE82tq-s830',
+  authDomain: 'launchday-mvp.firebaseapp.com',
+  projectId: 'launchday-mvp',
+  storageBucket: 'launchday-mvp.appspot.com',
+  messagingSenderId: '419424197757',
+  appId: '1:419424197757:web:89a775569b96670269a514',
+  measurementId: 'G-27J597W62Q',
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,15 @@
 import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
 
-import netlifyidentity from 'netlify-identity-widget';
-
 interface Props {}
 
 const Login = (props: Props) => {
   return (
     <Flex align="center" justify="center">
-      <Button
-        mx={2}
-        onClick={() => {
-          netlifyidentity.open('login');
-        }}
-      >
+      <Button as={'a'} mx={2} href={'/'}>
         Log In
       </Button>
-      <Button
-        mx={2}
-        onClick={() => {
-          netlifyidentity.open('signup');
-        }}
-      >
+      <Button as={'a'} mx={2} href={'/sign-up'}>
         Sign Up
       </Button>
     </Flex>
