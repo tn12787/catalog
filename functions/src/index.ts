8042d1fa4078be96554a3bf5fb792ac9680ebfc0
Sebastian Carroll <seabass25@hotmail.com>
2020-12-29 16:10:43 +0000

    Added firestore function uppercase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,3 @@
-// import * as functions from 'firebase-functions';
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 import * as functions from 'firebase-functions';
 
@@ -25,3 +15,21 @@ exports.addMessage = functions.https.onRequest(async (req: functions.Request, re
     // Send back a message that we've successfully written the message
     res.json({result: `Message with ID: ${writeResult.id} added.`});
   });
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+.onCreate((snap, context) => {
+  // Grab the current value of what was written to Cloud Firestore.
+  const original = snap.data().original;
+
+  // Access the parameter `{documentId}` with `context.params`
+  functions.logger.log('Uppercasing', context.params.documentId, original);
+  
+  const uppercase = original.toUpperCase();
+  
+  // You must return a Promise when performing asynchronous tasks inside a Functions such as
+  // writing to Cloud Firestore.
+  // Setting an 'uppercase' field in Cloud Firestore document returns a Promise.
+  return snap.ref.set({uppercase}, {merge: true});
+});
