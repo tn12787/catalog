55fcf1d33f3c797e5e5a38b52184b32ebd0884fa
tn12787 <tpjnorton@hotmail.co.uk>
2021-09-08 22:18:20 +0200

    add mobile boi

--- src/components/layouts/DashboardLayout/index.tsx
+++ src/components/layouts/DashboardLayout/index.tsx
@@ -1,12 +1,52 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Input,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { appLinks } from 'appLinks';
 import Nav from 'components/Nav';
+import useAppColors from 'hooks/useAppColors';
 import React from 'react';
+import { BiMenu } from 'react-icons/bi';
 
 const DashboardLayout: React.FC = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { bgPrimary } = useAppColors();
   return (
     <Flex minH="100vh">
-      <Nav links={appLinks} />
+      <Box display={{ base: 'none', md: 'flex' }}>
+        <Nav links={appLinks} />
+      </Box>
+      <Box display={{ base: 'block', md: 'none' }}>
+        <IconButton
+          aria-label="open menu"
+          icon={<BiMenu />}
+          rounded="full"
+          position="fixed"
+          bottom={20}
+          right={20}
+          colorScheme="purple"
+          onClick={onOpen}
+        >
+          Open
+        </IconButton>
+        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent maxW="300px" bg={bgPrimary}>
+            <Nav links={appLinks} />
+          </DrawerContent>
+        </Drawer>
+      </Box>
       <Flex width="100%" ml={{ base: 0, md: '300px' }} overflowY="auto">
         {children}
       </Flex>
