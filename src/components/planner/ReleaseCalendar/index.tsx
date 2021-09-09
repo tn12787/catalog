45c0c7bd1c3dc48e99af77b0324d4fd68c307313
tn12787 <tpjnorton@hotmail.co.uk>
2021-09-09 13:24:38 +0200

    do some needed cleanup

---
+++ src/components/planner/ReleaseCalendar/index.tsx
@@ -0,0 +1,104 @@
+import { useToast, useDisclosure } from '@chakra-ui/react';
+import Calendar from 'components/Calendar';
+import UndoToast from 'components/Calendar/UndoToast';
+import dayjs from 'dayjs';
+import { updateEventInCalendar } from 'queries/events';
+import React from 'react';
+import { useQueryClient, useMutation } from 'react-query';
+import { ReleaseEvent } from 'types';
+import { cloneDeep } from 'lodash';
+import ReleaseEventDrawer from '../ReleaseEventDrawer';
+
+interface Props {
+  events: ReleaseEvent[];
+  loading?: boolean;
+}
+
+const ReleaseCalendar = ({ events, loading }: Props) => {
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: updateReleaseEvent } = useMutation(
+    updateEventInCalendar,
+    {
+      onMutate: async ({ event, targetDate }) => {
+        await queryClient.cancelQueries('releaseEvents');
+        const events = queryClient.getQueryData('releaseEvents');
+
+        const data = cloneDeep(events) as ReleaseEvent[];
+        data?.forEach((item) => {
+          if (item.data.id === event.data.id) {
+            item.date = targetDate;
+          }
+        });
+
+        await queryClient.setQueryData('releaseEvents', data);
+
+        return { events };
+      },
+
+      onError: (err, newTodo, context: any) => {
+        queryClient.setQueryData('releaseEvents', context?.events);
+      },
+
+      onSettled: (_, __, ___, context: any) => {
+        queryClient.invalidateQueries(['releaseEvents']);
+      },
+    }
+  );
+
+  const toast = useToast();
+
+  const onUndo = async (event: ReleaseEvent) => {
+    await updateReleaseEvent({
+      event,
+      targetDate: event.date,
+    });
+  };
+
+  const onItemDropped = async (item: ReleaseEvent, targetDate: Date) => {
+    const backupItem = cloneDeep(item);
+    try {
+      await updateReleaseEvent({
+        event: item,
+        targetDate: dayjs.utc(targetDate).toISOString(),
+      });
+      toast.closeAll();
+      toast({
+        status: 'success',
+        title: 'Event updated',
+        duration: 4000,
+        // eslint-disable-next-line react/display-name
+        render: ({ onClose }) => (
+          <UndoToast onClose={onClose} onUndo={() => onUndo(backupItem)} />
+        ),
+      });
+    } catch (e: any) {
+      console.log(e);
+      toast({
+        status: 'error',
+        title: 'Oh no...',
+        description: e.toString(),
+      });
+    }
+  };
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const onCalendarEventClicked = (event: ReleaseEvent) => {
+    onOpen();
+  };
+
+  return (
+    <>
+      <ReleaseEventDrawer isOpen={isOpen} onClose={onClose} />
+      <Calendar
+        events={events}
+        loading={loading}
+        onEventClicked={onCalendarEventClicked}
+        onEventDropped={onItemDropped}
+      />
+    </>
+  );
+};
+
+export default ReleaseCalendar;
--- src/pages/planner/index.tsx
+++ src/pages/planner/index.tsx
@@ -11,41 +11,24 @@ import {
   TabPanels,
   Tabs,
   Text,
-  useDisclosure,
-  useToast,
 } from '@chakra-ui/react';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 interface Props {}
 import { getServerSideSessionOrRedirect } from 'ssr/getServerSideSessionOrRedirect';
 
 import useAppColors from 'hooks/useAppColors';
-import { fetchReleaseEvents, updateEventInCalendar } from 'queries/events';
-import Calendar from 'components/Calendar';
+import { fetchReleaseEvents } from 'queries/events';
 import { ReleaseEvent } from 'types';
-import UndoToast from 'components/Calendar/UndoToast';
-import dayjs from 'dayjs';
-import { cloneDeep } from 'lodash';
-import ReleaseEventDrawer from 'components/planner/ReleaseEventDrawer';
+import ReleaseCalendar from 'components/planner/ReleaseCalendar';
 
 const tabData = (
   events: ReleaseEvent[],
-  onCalendarEventClicked: (event: ReleaseEvent) => void,
-  onEventDropped: (
-    event: ReleaseEvent,
-    targetDate: Date
-  ) => void | Promise<void>,
+
   isLoading: boolean
 ) => [
   {
     label: 'Calendar View',
-    content: (
-      <Calendar
-        events={events}
-        loading={isLoading}
-        onEventClicked={onCalendarEventClicked}
-        onEventDropped={onEventDropped}
-      />
-    ),
+    content: <ReleaseCalendar events={events} loading={isLoading} />,
   },
   { label: 'List View', content: <Text>List View</Text> },
 ];
@@ -57,85 +40,7 @@ const Planner = (props: Props) => {
     fetchReleaseEvents
   );
 
-  const queryClient = useQueryClient();
-
-  const { mutateAsync: updateReleaseEvent } = useMutation(
-    updateEventInCalendar,
-    {
-      onMutate: async ({ event, targetDate }) => {
-        await queryClient.cancelQueries('releaseEvents');
-        const events = queryClient.getQueryData('releaseEvents');
-
-        const data = cloneDeep(events) as ReleaseEvent[];
-        data?.forEach((item) => {
-          if (item.data.id === event.data.id) {
-            item.date = targetDate;
-          }
-        });
-
-        await queryClient.setQueryData('releaseEvents', data);
-
-        return { events };
-      },
-
-      onError: (err, newTodo, context: any) => {
-        queryClient.setQueryData('releaseEvents', context?.events);
-      },
-
-      onSettled: (_, __, ___, context: any) => {
-        queryClient.invalidateQueries(['releaseEvents']);
-      },
-    }
-  );
-
-  const toast = useToast();
-
-  const onUndo = async (event: ReleaseEvent) => {
-    await updateReleaseEvent({
-      event,
-      targetDate: event.date,
-    });
-  };
-
-  const onItemDropped = async (item: ReleaseEvent, targetDate: Date) => {
-    const backupItem = cloneDeep(item);
-    try {
-      await updateReleaseEvent({
-        event: item,
-        targetDate: dayjs.utc(targetDate).toISOString(),
-      });
-      toast.closeAll();
-      toast({
-        status: 'success',
-        title: 'Event updated',
-        duration: 4000,
-        // eslint-disable-next-line react/display-name
-        render: ({ onClose }) => (
-          <UndoToast onClose={onClose} onUndo={() => onUndo(backupItem)} />
-        ),
-      });
-    } catch (e: any) {
-      console.log(e);
-      toast({
-        status: 'error',
-        title: 'Oh no...',
-        description: e.toString(),
-      });
-    }
-  };
-
-  const { isOpen, onClose, onOpen } = useDisclosure();
-
-  const onCalendarEventClicked = (event: ReleaseEvent) => {
-    onOpen();
-  };
-
-  const tabsToRender = tabData(
-    data ?? [],
-    onCalendarEventClicked,
-    onItemDropped,
-    isLoading
-  );
+  const tabsToRender = tabData(data ?? [], isLoading);
 
   return (
     <Stack
@@ -147,7 +52,6 @@ const Planner = (props: Props) => {
       width="100%"
     >
       <Stack spacing={4} width="95%" maxW="container.full">
-        <ReleaseEventDrawer isOpen={isOpen} onClose={onClose} />
         <Stack w="100%" alignItems="center">
           <Tabs w="100%" align="center" colorScheme="purple">
             <Stack
