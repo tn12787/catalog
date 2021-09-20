d9adf821de804aa222e4337a60453836c80e16bd
tn12787 <tpjnorton@hotmail.co.uk>
2021-09-20 12:58:59 +0200

    fix color flashing

---
+++ src/components/ChakraSSRProvider/index.tsx
@@ -0,0 +1,33 @@
+// e.g. src/Chakra.js
+// a) import `ChakraProvider` component as well as the storageManagers
+import {
+  cookieStorageManager,
+  localStorageManager,
+} from '@chakra-ui/color-mode';
+import { ChakraProvider } from '@chakra-ui/provider';
+import { GetServerSideProps } from 'next';
+
+import { appTheme } from 'customTheme';
+
+const ChakraSSRProvider = ({ cookies, children }: any) => {
+  const colorModeManager =
+    typeof cookies === 'string'
+      ? cookieStorageManager(cookies)
+      : localStorageManager;
+
+  return (
+    <ChakraProvider colorModeManager={colorModeManager} theme={appTheme}>
+      {children}
+    </ChakraProvider>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  return {
+    props: {
+      cookies: req.headers.cookie ?? '',
+    },
+  };
+};
+
+export default ChakraSSRProvider;
