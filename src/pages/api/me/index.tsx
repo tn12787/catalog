e821984627554b875865a401bd4cf55f758f87eb
tn12787 <tpjnorton@hotmail.co.uk>
2022-01-21 15:51:38 +0100

    stop importing next in releases and 'me'

--- src/pages/api/me/index.tsx
+++ src/pages/api/me/index.tsx
@@ -1,16 +1,16 @@
 import { createHandler, Get, Request } from '@storyofams/next-api-decorators';
-import { NextApiRequest } from 'next';
 import { Session } from 'next-auth';
 import { getSession } from 'next-auth/react';
 import { JWT } from 'next-auth/jwt';
 
+import { AuthDecoratedRequest } from 'types/common';
 import { requiresAuth } from 'backend/apiUtils/decorators/auth';
 import prisma from 'backend/prisma/client';
 
 @requiresAuth()
 class MeHandler {
   @Get()
-  async me(@Request() req: NextApiRequest) {
+  async me(@Request() req: AuthDecoratedRequest) {
     const session = (await getSession({ req })) as Session & { token: JWT };
 
     const user = await prisma.user.findUnique({
