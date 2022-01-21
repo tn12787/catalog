1313c2d3b252adad7adef0636130c307e3982b41
tn12787 <tpjnorton@hotmail.co.uk>
2022-01-21 15:02:36 +0100

    try disabling state for now

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      checks: ['pkce'],
       authorization: {
         params: {
           scope:
