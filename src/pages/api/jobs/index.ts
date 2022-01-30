468de2a6792e0a15a647b05e09a09a377a37ceb1
Sebastian Carroll <seabass25@hotmail.com>
2022-01-30 11:36:44 +0000

    First pass at getting task notifications

--- src/pages/api/jobs/index.ts
+++ src/pages/api/jobs/index.ts
@@ -1,12 +1,31 @@
-import { createHandler, Post } from '@storyofams/next-api-decorators';
+import { TaskStatus } from '@prisma/client';
+import { createHandler, Get } from '@storyofams/next-api-decorators';
 
-import { requiresServiceAccount } from 'backend/apiUtils/decorators/auth';
+// import { requiresServiceAccount } from 'backend/apiUtils/decorators/auth';
+import prisma from 'backend/prisma/client';
 
-@requiresServiceAccount()
+// TODO: Reenable service account access
+//@requiresServiceAccount()
 class JobHandler {
-  @Post()
+  @Get() // TODO: Change back to post after testing
   async runJob() {
-    return { acknowledged: true };
+    // Here do something with prisma that finds tasks that are potentiall overdue or almost due and write a notification for them
+
+    // Get all tasks with due date < today +2 and not completed yet
+    // TaskStatus != COMPLETE
+
+    const releasetasks = prisma.releaseTask.findMany({
+      where: { 
+        status: {
+          not: TaskStatus.COMPLETE
+        }
+      }
+    });
+
+    return {
+      acknowledged: true, 
+      releasetasks 
+    };
   }
 }
 
