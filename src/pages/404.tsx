fc0e7e9ec91d0f787297b53cffa5cd07bbd3a3be
tn12787 <tpjnorton@hotmail.co.uk>
2022-03-05 13:54:57 +0100

    add nicer 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,20 +1,35 @@
-import { Flex, Stack, Heading, Text } from '@chakra-ui/react';
+import { Flex, Stack, Heading, Text, ListItem, UnorderedList, Button } from '@chakra-ui/react';
 import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import PageHead from 'components/pageItems/PageHead';
 import useAppColors from 'hooks/useAppColors';
+import notFound from 'images/not-found.svg';
 
 const NotFound = () => {
   const { bgPrimary } = useAppColors();
+  const router = useRouter();
   return (
     <Flex bg={bgPrimary} direction="column" align="center" justify="center" flex={1} minH="100vh">
       <PageHead title="Home" />
-      <Stack w={'80%'} maxW="400px" spacing={8} alignItems="center">
+      <Stack w={'80%'} maxW="350px" spacing={6} alignItems="center">
+        <Image src={notFound} alt="No artists yet" />
         <Heading fontWeight="semibold" fontSize="8xl">
           404
         </Heading>
-
-        <Text>{"The page you're looking for wasn't found."}</Text>
+        <Text>{"The thing you're looking for wasn't found."}</Text>
+        <Stack alignSelf="flex-start">
+          <Text fontWeight={'semibold'}>Possible causes:</Text>
+          <UnorderedList fontSize={'sm'}>
+            <ListItem>{"You're signed in to the wrong Workspace."}</ListItem>
+            <ListItem>You typed an invalid URL into your browser.</ListItem>
+            <ListItem>{"The thing you're looking for no longer exists."}</ListItem>
+          </UnorderedList>
+        </Stack>
+        <Button variant={'link'} onClick={() => router.back()} color={'purple.500'}>
+          Go back
+        </Button>
       </Stack>
     </Flex>
   );
