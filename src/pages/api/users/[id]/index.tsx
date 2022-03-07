43511325fefe684389bddf89401481897edf3ad8
tn12787 <tpjnorton@hotmail.co.uk>
2022-03-07 03:14:01 +0100

    only allow users to update themselves

--- src/pages/api/users/[id]/index.tsx
+++ src/pages/api/users/[id]/index.tsx
@@ -1,14 +1,29 @@
-import { Body, createHandler, Put, ValidationPipe } from '@storyofams/next-api-decorators';
+import {
+  Body,
+  createHandler,
+  NotFoundException,
+  Put,
+  Request,
+  ValidationPipe,
+} from '@storyofams/next-api-decorators';
 
 import { requiresAuth } from 'backend/apiUtils/decorators/auth';
 import prisma from 'backend/prisma/client';
 import { PathParam } from 'backend/apiUtils/decorators/routing';
 import { UpdateUserDto } from 'backend/models/users/update';
+import { AuthDecoratedRequest } from 'types/common';
 
 @requiresAuth()
 class UserHandler {
   @Put()
-  async updateUser(@PathParam('id') id: string, @Body(ValidationPipe) body: UpdateUserDto) {
+  async updateUser(
+    @PathParam('id') id: string,
+    @Request() req: AuthDecoratedRequest,
+    @Body(ValidationPipe) body: UpdateUserDto
+  ) {
+    const userId = req.session.token.sub;
+    if (id !== userId) throw new NotFoundException();
+
     const user = await prisma.user.update({
       where: { id },
       data: {
