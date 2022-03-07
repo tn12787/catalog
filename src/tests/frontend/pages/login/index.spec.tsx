a84162b3408d0a877b909343592da19197999064
tn12787 <tpjnorton@hotmail.co.uk>
2022-03-07 18:06:42 +0100

    fix test

--- src/tests/frontend/pages/login/index.spec.tsx
+++ src/tests/frontend/pages/login/index.spec.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import LoginPage from 'pages/login';
 import { renderWithProviders } from 'tests/utils/render';
 
@@ -5,6 +7,9 @@ const render = () => renderWithProviders(<LoginPage />);
 
 describe('Login Page', () => {
   it('Shows a login page', () => {
+    beforeAll(() => {
+      (useRouter as jest.Mock).mockImplementation(() => ({ query: { callbackUrl: 'boi' } }));
+    });
     const { getByText } = render();
     expect(getByText(/Log in to/)).toBeVisible();
   });
