53963e60ab929a41107d9801429d4293dabbc2dd
Tom Norton <tpjnorton@hotmail.co.uk>
2022-03-08 13:15:28 +0100

    Hide Spotify & Slack Logins Behind Feature Flags (#63)
    * hide logins behind feature flags for now
    
    * clean up string

--- src/common/features/index.ts
+++ src/common/features/index.ts
@@ -2,6 +2,8 @@ import { AppFeatureMap, FeatureKey } from './types';
 
 export const staticFeatures: AppFeatureMap = {
   [FeatureKey.PAYMENTS]: false,
+  [FeatureKey.SPOTIFY_LOGIN]: false,
+  [FeatureKey.SLACK_LOGIN]: false,
 };
 
 export const isBackendFeatureEnabled = (feature: FeatureKey): boolean => {
--- src/common/features/types.ts
+++ src/common/features/types.ts
@@ -1,5 +1,7 @@
 export enum FeatureKey {
   PAYMENTS = 'payments',
+  SPOTIFY_LOGIN = 'spotifyLogin',
+  SLACK_LOGIN = 'slackLogin',
 }
 
 export type AppFeatureMap = Record<FeatureKey, boolean>;
