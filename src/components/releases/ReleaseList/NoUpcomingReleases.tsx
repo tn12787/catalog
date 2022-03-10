2c9058dacad1baf1a33a44df9aa9cc722ea6df64
tn12787 <tpjnorton@hotmail.co.uk>
2022-03-10 19:53:25 +0100

    remove stuff

--- src/components/releases/ReleaseList/NoUpcomingReleases.tsx
+++ src/components/releases/ReleaseList/NoUpcomingReleases.tsx
@@ -1,11 +1,8 @@
-import Link from 'next/link';
 import React from 'react';
-import { Button, Icon } from '@chakra-ui/react';
+import { Icon } from '@chakra-ui/react';
 import { BiDisc } from 'react-icons/bi';
 
 import NoReleasesSubtle from 'components/releases/ReleaseList/NoReleasesSubtle';
-import { hasRequiredPermissions } from 'utils/auth';
-import useExtendedSession from 'hooks/useExtendedSession';
 
 const NoUpcomingReleases = () => {
   return (
