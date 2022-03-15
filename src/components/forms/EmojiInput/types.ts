e3b015af0588fab91bf2fcfb4757d88239fffa2c
tn12787 <tpjnorton@hotmail.co.uk>
2022-03-15 17:01:44 +0100

    fix janky emoji picker behavior

---
+++ src/components/forms/EmojiInput/types.ts
@@ -0,0 +1,4 @@
+export type EmojiValue = {
+  emoji: string;
+  text: string;
+};
--- src/components/releases/forms/TaskForm/TaskFormBody.tsx
+++ src/components/releases/forms/TaskForm/TaskFormBody.tsx
@@ -6,6 +6,7 @@ import { addDays, format, startOfDay } from 'date-fns';
 
 import { buildTaskConfig } from './taskConfig';
 import { TaskFormData } from './types';
+import { deriveEmojiValueData } from './utils';
 
 import FormContent from 'components/forms/FormContent';
 import { FormBodyProps } from 'types/forms';
@@ -16,30 +17,13 @@ interface Props extends FormBodyProps<TaskFormData> {
   generic?: boolean;
 }
 
-const baseEmojiRegex =
-  '[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]';
-
-const emojiRegex = new RegExp(`(${baseEmojiRegex}+)(.*)`, 'u');
-
 const TaskFormBody = ({ onSubmit, loading, existingData, generic }: Props) => {
   const properDateFormat = useMemo(() => {
     const existingDate = existingData?.dueDate ?? addDays(startOfDay(new Date()), 7);
     return format(new Date(existingDate), 'yyyy-MM-dd');
   }, [existingData?.dueDate]);
 
-  const { emoji, nameText } = useMemo(() => {
-    if (!existingData?.name) return { emoji: '', nameText: '' };
-
-    const defaultReturn = { emoji: '', nameText: existingData.name };
-
-    const startsWithEmoji = emojiRegex.test(existingData.name);
-    if (!startsWithEmoji) {
-      return defaultReturn;
-    }
-
-    const [, emoji, stringValue] = emojiRegex.exec(existingData.name) ?? [];
-    return { emoji, nameText: stringValue?.trimStart() } ?? defaultReturn;
-  }, [existingData]);
+  const mappedTaskName = useMemo(() => deriveEmojiValueData(existingData?.name), [existingData]);
 
   const {
     register,
@@ -50,7 +34,7 @@ const TaskFormBody = ({ onSubmit, loading, existingData, generic }: Props) => {
     defaultValues: {
       ...existingData,
       dueDate: properDateFormat as any,
-      name: { emoji: emoji, text: nameText },
+      name: mappedTaskName,
     },
   });
 
