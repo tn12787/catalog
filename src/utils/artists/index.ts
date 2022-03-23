2ca61a8fd0c0b970b1e08c13d2c893294c87747b
tn12787 <tpjnorton@hotmail.co.uk>
2022-03-23 18:23:09 +0100

    restrict adding artists on API

--- src/utils/artists/index.ts
+++ src/utils/artists/index.ts
@@ -1,5 +1,7 @@
+import { isBackendFeatureEnabled } from 'common/features';
 import { PlanName } from 'types/marketing/pricing';
 import { EnrichedWorkspace } from 'types/common';
+import { FeatureKey } from 'common/features/types';
 
 export const artistLimits: Record<PlanName, number> = {
   artist: 2,
@@ -21,5 +23,7 @@ export const getLimitForSubscription = (
 };
 
 export const canAddAnotherArtist = (count: number, workspace?: EnrichedWorkspace) => {
+  if (!isBackendFeatureEnabled(FeatureKey.PAYMENTS)) return true;
+
   return count < getLimitForSubscription(workspace?.subscription);
 };
