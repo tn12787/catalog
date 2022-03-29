08498a6f11b57b1102c0e97ad504c349b56ab9a2
Tom <tpjnorton@hotmail.co.uk>
2022-03-29 17:07:18 +0200

    include google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,12 +8,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="icon" href={`${process.env.PUBLIC_URL}/favicon.ico`} />
-          <meta name="theme-color" content="#000000" />
           <script src="https://apis.google.com/js/api.js" async />
-          <link rel="apple-touch-icon" href={`${process.env.PUBLIC_URL}/logo192.png`} />
-          <link rel="manifest" href={`${process.env.PUBLIC_URL}/manifest.json`} />
-          <noscript>You need to enable JavaScript to run this app.</noscript>
         </Head>
 
         <body>
