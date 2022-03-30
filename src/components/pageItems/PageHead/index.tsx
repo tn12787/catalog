69c06e0f872209ff56fc164a68451be66aeb97d6
Tom <tpjnorton@hotmail.co.uk>
2022-03-30 12:45:44 +0200

    fix more stuff

--- src/components/pageItems/PageHead/index.tsx
+++ src/components/pageItems/PageHead/index.tsx
@@ -2,20 +2,20 @@ import Head from 'next/head';
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import og_image from 'images/og_image.png';
-
 interface Props {
   title: string;
+  titleTemplate?: string;
   description?: string;
   image?: string;
 }
 
 const PageHead = ({
   title,
+  titleTemplate = ' | Catalog',
   description = 'First-rate tools for managing music releases.',
-  image = og_image,
+  image = '/og_image.png',
 }: Props) => {
-  const titleTemplate = `${title} | Catalog`;
+  const derivedTitle = `${title}${titleTemplate}`;
   const router = useRouter();
   return (
     <Head>
@@ -28,14 +28,19 @@ const PageHead = ({
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
 
-      <title key="title">{titleTemplate}</title>
+      <title key="title">{derivedTitle}</title>
       <meta name="description" content={description} />
 
       <meta property="og:url" content={router.asPath} key="ogurl" />
       <meta property="og:image" content={image} key="ogimage" />
+      <meta property="og:image:url" content={image} />
+      <meta property="og:image:width" content={'1200'} key="ogimagewidth" />
+      <meta property="og:image:height" content={'630'} key="ogimageheight" />
       <meta property="og:site_name" content={'Catalog'} key="ogsitename" />
-      <meta property="og:title" content={titleTemplate} key="ogtitle" />
+      <meta property="og:title" content={derivedTitle} key="ogtitle" />
       <meta property="og:description" content={description} key="ogdesc" />
+
+      <meta property="twitter:image" content="image" key="twitterImage" />
     </Head>
   );
 };
