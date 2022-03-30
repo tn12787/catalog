f73c96e0517934f5106f09f055703e342dbff8cb
Tom Norton <tpjnorton@hotmail.co.uk>
2022-03-30 17:02:22 +0200

    Production-ize Google Sign-in (#144)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -27,8 +27,7 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events',
+          scope: 'openid profile email',
         },
       },
     }),
