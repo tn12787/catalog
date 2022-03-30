d52e2ca2014b68a47e777a2112e3fafe3fdd7ab1
tn12787 <tpjnorton@hotmail.co.uk>
2022-03-30 15:45:57 +0200

    try hotfixing middleware

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,3 +1,21 @@
-import sslRedirect from 'next-ssl-redirect-middleware';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-export default sslRedirect({});
+type Environment = 'production' | 'development' | 'other';
+
+type Params = {
+  environments?: Environment[];
+  status?: 301 | 302;
+};
+
+const middleware =
+  ({ environments = ['production', 'development'], status = 301 }: Params) =>
+  (req: NextRequest, ev: NextFetchEvent) => {
+    const currentEnv = process.env.NODE_ENV as Environment;
+
+    if (environments.includes(currentEnv) && req.headers.get('x-forwarded-proto') !== 'https') {
+      return NextResponse.redirect(`https://${req.nextUrl.host}${req.nextUrl.pathname}`, status);
+    }
+    return NextResponse.next();
+  };
+
+export default middleware({});
