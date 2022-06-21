6e0b9fb610335b0d37cfbd53d4bb459156257d42
tn12787 <tpjnorton@hotmail.co.uk>
2022-06-21 15:30:03 +0200

    set up analytics provider

---
+++ src/analytics/ga/AnalyticsProvider/index.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
+import ga from '..';
+
+import { ExtendedSession } from 'types/auth';
+
+const AnalyticsProvider: React.FC = ({ children }) => {
+  const { data } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    const session = data as ExtendedSession;
+    session && ga.trackIdentity(session.token.sub);
+  }, [data]);
+
+  return <>{children}</>;
+};
+
+export default AnalyticsProvider;
