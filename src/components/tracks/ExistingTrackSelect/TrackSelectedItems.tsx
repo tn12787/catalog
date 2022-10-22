5a286ff6aa9effee54964c14318f0103c7f8c955
tn12787 <tpjnorton@hotmail.co.uk>
2022-10-22 19:13:36 +0200

    polish up track-linking experience

--- src/components/tracks/ExistingTrackSelect/TrackSelectedItems.tsx
+++ src/components/tracks/ExistingTrackSelect/TrackSelectedItems.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Wrap, Text } from '@chakra-ui/react';
+import { Text, Stack, HStack, IconButton } from '@chakra-ui/react';
+import { BiX } from 'react-icons/bi';
 
 import { SelectedItemListProps } from 'components/forms/MultiSelect/types';
 import { TrackResponse } from 'types/common';
+import useAppColors from 'hooks/useAppColors';
 
-const TrackSelectedItems = ({ items }: SelectedItemListProps<TrackResponse>) => {
+const TrackSelectedItems = ({ items, onChange }: SelectedItemListProps<TrackResponse>) => {
+  const { bodySub } = useAppColors();
   return (
-    <Wrap>
+    <Stack spacing={3} py={3}>
       {items?.length
-        ? items?.map((label) => {
-            return <Text key={label.id}>{label.name}</Text>;
+        ? items?.map((track) => {
+            return (
+              <HStack justify={'space-between'} key={track.id}>
+                <Stack spacing={0}>
+                  <Text fontSize="sm" fontWeight={'medium'}>
+                    {track.name}
+                  </Text>
+                  <Text fontSize="xs" color={bodySub}>
+                    {track.mainArtists.map((a) => a.name).join(', ')}
+                  </Text>
+                </Stack>
+                <IconButton
+                  onClick={() => {
+                    onChange(items?.filter((item) => item?.id !== track.id));
+                  }}
+                  size="xs"
+                  icon={<BiX></BiX>}
+                  aria-label={'remove track'}
+                ></IconButton>
+              </HStack>
+            );
           })
         : null}
-    </Wrap>
+    </Stack>
   );
 };
 
--- src/components/tracks/ExistingTrackSelect/index.tsx
+++ src/components/tracks/ExistingTrackSelect/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ControllerRenderProps } from 'react-hook-form';
 import { Track } from '@prisma/client';
+import { Text } from '@chakra-ui/layout';
 
 import TrackItem from './TrackItem';
 import TrackSelectedItems from './TrackSelectedItems';
@@ -9,15 +10,21 @@ import { MultiSelectListItemProps } from 'components/forms/MultiSelect/types';
 import MultiSelect from 'components/forms/MultiSelect';
 import useWorkspaceTracks from 'hooks/data/tracks/useWorkspaceTracks';
 import { TrackResponse } from 'types/common';
+import useAppColors from 'hooks/useAppColors';
 
 interface Props extends Pick<ControllerRenderProps, 'onChange'> {
   value: TrackResponse[];
   borderless?: boolean;
+  existingTracks: Track[];
 }
 
 const ExistingTrackSelect: React.FC<Props> = React.forwardRef<HTMLSelectElement, Props>(
-  ({ value, borderless, onChange }: Props, ref) => {
+  ({ value, borderless, onChange, existingTracks }: Props, ref) => {
     const { data: allTracks, isLoading } = useWorkspaceTracks({});
+    const { bodySub } = useAppColors();
+
+    const availableTracks =
+      allTracks?.results?.filter((t) => !existingTracks.find((et) => et.id === t.id)) ?? [];
 
     return (
       <MultiSelect
@@ -26,13 +33,18 @@ const ExistingTrackSelect: React.FC<Props> = React.forwardRef<HTMLSelectElement,
         value={value}
         onChange={onChange}
         borderless={borderless}
+        emptyContent={
+          <Text color={bodySub} fontSize="sm">
+            No tracks matched your search, or there are no more tracks in your workspace to add.
+          </Text>
+        }
         itemToString={(item: Track) => item?.name || ''}
         renderSelectedItems={TrackSelectedItems}
         renderListItem={(props: MultiSelectListItemProps<TrackResponse>) => (
           <TrackItem {...props} />
         )}
         searchPlaceholder="Search for a track..."
-        allItems={allTracks?.results ?? []}
+        allItems={availableTracks}
         filterFn={(item: Track, search: string) =>
           item.name?.toLowerCase().includes(search.toLowerCase()) ?? false
         }
