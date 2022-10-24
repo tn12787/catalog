0be5a99289c1fe5ca545fe3ef3394b6df6670395
Tom <tpjnorton@hotmail.co.uk>
2022-10-24 15:41:30 +0200

    get track creation working

--- src/components/tracks/forms/TrackForm/CreateTrackForm/types.ts
+++ src/components/tracks/forms/TrackForm/CreateTrackForm/types.ts
@@ -1,8 +1,8 @@
-import { Track } from '@prisma/client';
+import { Artist, Track } from '@prisma/client';
 
 export type CreateTrackFormData = {
   name: Track['name'];
   lyrics: Track['lyrics'];
-  mainArtists: string[];
-  featuringArtists?: string[];
+  mainArtists: Artist[];
+  featuringArtists?: Artist[];
 };
