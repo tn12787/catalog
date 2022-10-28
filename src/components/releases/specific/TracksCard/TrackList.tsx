877f416327db31e8b88ae415d01aaf5394ac88eb
tn12787 <tpjnorton@hotmail.co.uk>
2022-10-28 15:46:33 +0200

    start making tracks draggable

--- src/components/releases/specific/TracksCard/TrackList.tsx
+++ src/components/releases/specific/TracksCard/TrackList.tsx
@@ -1,9 +1,9 @@
-import { Stack, Flex, Text, Wrap, Link } from '@chakra-ui/react';
+import { Stack, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
-import NextLink from 'next/link';
 
-import { TrackField } from './types';
 import TrackListEmpty from './TrackListEmpty';
+import TrackListItem from './TrackListItem';
+import { fields } from './fields';
 
 import { ClientRelease } from 'types/common';
 
@@ -11,56 +11,38 @@ type Props = {
   tracks: ClientRelease['tracks'];
 };
 
-const fields = (track: ClientRelease['tracks'][0]): TrackField[] => [
-  {
-    name: 'Name',
-    content: <Text fontSize="sm">{track.name}</Text>,
-  },
-  {
-    name: 'Artists',
-    content: (
-      <Wrap>
-        {track.mainArtists.map((artist) => (
-          <NextLink key={artist.id} href={`/artists/${artist.id}`} passHref>
-            <Link>{artist.name}</Link>
-          </NextLink>
-        ))}
-      </Wrap>
-    ),
-  },
-];
-
 const TrackList = ({ tracks }: Props) => {
   return (
     <Stack>
       {tracks.length ? (
-        tracks.map((track) => (
+        <Stack>
           <Flex
-            key={track.id}
             direction={['column', 'column', 'row']}
             width={'90%'}
             justify="space-between"
             alignItems={['center', 'center', 'stretch']}
           >
-            {fields(track).map((field) => {
+            {fields(tracks[0]).map((field) => {
               return (
                 <Flex
                   mb={[3, 3, 0]}
                   width="100%"
-                  align={['center', 'center', 'flex-start']}
-                  direction={['row', 'row', 'column']}
-                  justify={['space-between']}
+                  align={{ base: 'center', md: 'flex-start' }}
+                  direction={{ base: 'row', md: 'column' }}
+                  justify={{ base: 'space-between', md: 'center' }}
                   key={field.name}
                 >
                   <Text fontSize="sm" fontWeight="semibold">
                     {field.name}
                   </Text>
-                  {field.content}
                 </Flex>
               );
             })}
           </Flex>
-        ))
+          {tracks.map((track, index) => (
+            <TrackListItem track={track} index={index} key={track.id} />
+          ))}
+        </Stack>
       ) : (
         <TrackListEmpty></TrackListEmpty>
       )}
