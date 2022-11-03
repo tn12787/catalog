485473831b6c1e7fc0cb110313d5755d7c8c48f9
tn12787 <tpjnorton@hotmail.co.uk>
2022-11-03 15:45:46 +0100

    add some fluff

--- src/components/releases/specific/TracksCard/index.tsx
+++ src/components/releases/specific/TracksCard/index.tsx
@@ -10,6 +10,9 @@ import {
   ModalBody,
   ModalHeader,
   Stack,
+  HStack,
+  Badge,
+  Text,
 } from '@chakra-ui/react';
 import React from 'react';
 import { BiPlus } from 'react-icons/bi';
@@ -39,29 +42,37 @@ const TracksCard = ({ releaseData }: Props) => {
 
   return (
     <Card alignItems={['center', 'center', 'stretch']}>
-      <Flex align="center" justify="space-between" direction={['column', 'column', 'row']}>
-        <Stack
-          w="100%"
-          align="center"
-          justify={{ base: 'center', md: 'space-between' }}
-          direction={{ base: 'column', md: 'row' }}
-        >
-          <Heading fontWeight="semibold" fontSize="2xl">
-            Tracks
-          </Heading>
-          {canUpdateRelease && (
-            <Button
-              size="sm"
-              alignSelf={'center'}
-              variant="outline"
-              onClick={onOpen}
-              leftIcon={<BiPlus />}
-            >
-              Add a track
-            </Button>
-          )}
-        </Stack>
-      </Flex>
+      <Stack spacing={4}>
+        <Flex align="center" justify="space-between" direction={['column', 'column', 'row']}>
+          <Stack
+            w="100%"
+            align="center"
+            justify={{ base: 'center', md: 'space-between' }}
+            direction={{ base: 'column', md: 'row' }}
+          >
+            <HStack>
+              <Heading fontWeight="semibold" fontSize="2xl">
+                Tracks
+              </Heading>
+              <Badge colorScheme={'blue'} variant="subtle" size="xs">
+                New
+              </Badge>
+            </HStack>
+            {canUpdateRelease && (
+              <Button
+                size="sm"
+                alignSelf={'center'}
+                variant="outline"
+                onClick={onOpen}
+                leftIcon={<BiPlus />}
+              >
+                Add a track
+              </Button>
+            )}
+          </Stack>
+        </Flex>
+        <Text fontSize="sm">{"Add the tracks that'll appear on your release."}</Text>
+      </Stack>
       <TrackList releaseData={releaseData}></TrackList>
 
       <Modal size="2xl" isOpen={isOpen} onClose={onClose}>
